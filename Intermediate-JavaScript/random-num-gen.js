b5d68de1f3831b6901776516e83ca80e15863d45
ghazyayman <muhghazyayman@gmail.com>
2023-02-26 16:44:34 +0800

    test

--- Intermediate-JavaScript/random-num-gen.js
+++ Intermediate-JavaScript/random-num-gen.js
@@ -3,4 +3,12 @@ prompt("What is their name?");
 
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1; // 1-100
+
+if (loveScore > 70) {
+    alert("Your love score is " + loveScore + "%" + "You love each other like Kanye loves Kanye."); 
+} else {
+    alert("Your love score is " + loveScore + "%");
+}
+
 alert("Your love score is " + loveScore + "%");
+
