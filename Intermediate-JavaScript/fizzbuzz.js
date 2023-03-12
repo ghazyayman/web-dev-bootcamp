758c79f35da688f45f567793b45da0140f909e1d
ghazyayman <muhghazyayman@gmail.com>
2023-03-12 15:31:30 +0800

    lunch

--- Intermediate-JavaScript/fizzbuzz.js
+++ Intermediate-JavaScript/fizzbuzz.js
@@ -8,11 +8,11 @@ function fizzBuzz() {
     count++;
 
    if (count % 3 === 0 && count % 5 === 0) {
-       output.push("Fizz");
+       output.push("FizzBuzz");
    } else if (count % 3 === 0) {
-       output.push("Buzz");
+       output.push("Fizz");
    } else if (count % 5 === 0) {
-         output.push("FizzBuzz");
+         output.push("Buzz");
    } else {
     output.push(count);      
    }
