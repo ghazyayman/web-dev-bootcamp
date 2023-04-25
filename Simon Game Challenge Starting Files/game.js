2833fb4ff7c5900fd9ced00b23bc9f0be353e7a2
ghazyayman <muhghazyayman@gmail.com>
2023-04-26 00:29:23 +0800

    completed

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -26,44 +25,32 @@ $(".btn").click(function() {
   checkAnswer(userClickedPattern.length-1);
 });
 
-
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
           nextSequence();
         }, 1000);
       }
-
     } else {
-
-      console.log("wrong");
-
-      //1. In the sounds folder, there is a sound called wrong.mp3, play this sound if the user got one of the answers wrong.
       playSound("wrong");
-
-      //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
       $("body").addClass("game-over");
+      $("#level-title").text("You sucks man! Start over again");
+
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
 
-      //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
     }
-
 }
 
-function nextSequence() {
 
+function nextSequence() {
   userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
-
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -72,14 +59,20 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
