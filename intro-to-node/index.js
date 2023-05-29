c78d528a0fa9e50379a964af81968b2547e34b05
ghazyayman <muhghazyayman@gmail.com>
2023-05-29 16:01:41 +0800

    learned

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();     
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);
