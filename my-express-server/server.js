92cc2e785c6ccef27ace6dbbbafc668e70dedfb6
ghazyayman <muhghazyayman@gmail.com>
2023-06-01 21:33:11 +0800

    hello

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -3,9 +3,9 @@ const app = express();
 
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-    })
+app.get('/', function(request, response){
+    response.send("<h1>Hello World</h1>");
+});
 
 app.listen(3000, function(){
     console.log('Server is running on port 3000');
