b68ba1deeee89770733e45b84eaec27257f14c8e
ghazyayman <muhghazyayman@gmail.com>
2023-06-03 23:52:12 +0800

    add contact

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -7,6 +7,10 @@ app.get('/', function(request, response){
     response.send("<h1>Hello World</h1>");
 });
 
+app.get("/contact", function(req, res){
+    res.send("Contact me at: muhghazy09@gmail.com");
+})
+
 app.listen(3000, function(){
     console.log('Server is running on port 3000');
 });
