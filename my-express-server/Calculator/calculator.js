2bd1e4ba6994855b6a28f7e572d2136c646ba2d9
ghazyayman <muhghazyayman@gmail.com>
2023-06-05 16:47:13 +0800

    calc

---
+++ my-express-server/Calculator/calculator.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello World");
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
