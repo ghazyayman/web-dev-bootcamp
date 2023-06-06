3b60a1c0fb491d649ca03a5bbfcd162b680b6b04
ghazyayman <muhghazyayman@gmail.com>
2023-06-06 20:53:46 +0800

    path

--- my-express-server/Calculator/calculator.js
+++ my-express-server/Calculator/calculator.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
-});
+}); 
