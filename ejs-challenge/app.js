514a5309edae98634b66b7f02602bc061a5de38a
ghazyayman <muhghazyayman@gmail.com>
2023-08-21 22:24:36 +0800

    compose

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -28,6 +28,13 @@ app.get("/contact", function (req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+app.get("/compose", function (req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  console.log(req.body.postTitle);
+})
 
 app.get
 
